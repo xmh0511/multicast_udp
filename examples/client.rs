5ad314018668c4adaedda0dc3068cdd76f35029d
xmh0511 <970252187@qq.com>
2024-11-28 10:31:49 +0800

    first commit

---
+++ examples/client.rs
@@ -0,0 +1,11 @@
+use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
+fn main(){
+    let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,0)).unwrap();
+    let local_addr = sock.local_addr().unwrap();
+    println!("local {local_addr}");
+    sock.join_multicast_v4(&Ipv4Addr::from([234,2,2,2]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    for _ in 0..30{
+        sock.send_to(b"hello", SocketAddr::from(([234,2,2,2],8888))).unwrap();
+    }
+    println!("send over");
+}
