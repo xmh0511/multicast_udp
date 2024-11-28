393e90c7af46a922944f80ac8b6fa28322a6101a
xmh0511 <970252187@qq.com>
2024-11-28 10:57:44 +0800

    u

--- examples/client.rs
+++ examples/client.rs
@@ -3,9 +3,9 @@ fn main(){
     let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,9090)).unwrap();
     let local_addr = sock.local_addr().unwrap();
     println!("local {local_addr}");
-    //sock.join_multicast_v4(&Ipv4Addr::from([234,2,2,2]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    sock.join_multicast_v4(&Ipv4Addr::from([234,2,2,2]), &Ipv4Addr::UNSPECIFIED).unwrap();
     for _ in 0..30{
-        sock.send_to(b"hello", SocketAddr::from(([234,2,2,2],8888))).unwrap();
+        sock.send_to(b"hello", SocketAddr::from(([234,2,2,2],9090))).unwrap();
     }
     println!("send over");
     sock.send_to(b"hello dedicated", SocketAddr::from(([192,168,1,158],9090))).unwrap();
