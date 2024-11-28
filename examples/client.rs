b61f89bbc34b9cba2a618db1e495f473ebb3ac42
xmh0511 <970252187@qq.com>
2024-11-28 11:27:02 +0800

    u

--- examples/client.rs
+++ examples/client.rs
@@ -5,8 +5,10 @@ fn main(){
     println!("local {local_addr}");
     sock.set_multicast_loop_v4(true).unwrap();
     sock.join_multicast_v4(&Ipv4Addr::from([239,255,42,98]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    std::thread::sleep(std::time::Duration::from_millis(5000));
     for _ in 0..100{
         sock.send_to(b"hello", SocketAddr::from(([239,255,42,98],9090))).unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(500));
     }
     println!("send over");
     sock.send_to(b"hello dedicated", SocketAddr::from(([192,168,1,158],9090))).unwrap();
