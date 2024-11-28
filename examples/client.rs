480483dc271a3949aaaf11ea67430ee8753e6e75
xmh0511 <970252187@qq.com>
2024-11-28 11:46:45 +0800

    u

--- examples/client.rs
+++ examples/client.rs
@@ -2,15 +2,15 @@ use std::{io::Write, net::{Ipv4Addr, SocketAddr, UdpSocket}};
 fn main(){
     let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,9090)).unwrap();
     let local_addr = sock.local_addr().unwrap();
-    println!("239,255,42,98 is mulitcast {}",Ipv4Addr::from([239,255,42,98]).is_multicast());
+    println!("224,0,2,1 is mulitcast {}",Ipv4Addr::from([224,0,2,1]).is_multicast());
     println!("local {local_addr}");
     sock.set_broadcast(true).unwrap();
     sock.set_multicast_loop_v4(true).unwrap();
-    sock.join_multicast_v4(&Ipv4Addr::from([239,255,42,98]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    sock.join_multicast_v4(&Ipv4Addr::from([224,0,2,1]), &Ipv4Addr::UNSPECIFIED).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(5000));
     for i in 0..100{
-        sock.send_to(b"hello multicast", SocketAddr::from(([239,255,42,98],9090))).unwrap();
-        sock.send_to(b"hello broadcast", SocketAddr::from(([255,255,255,255],9090))).unwrap();
+        sock.send_to(b"hello multicast", SocketAddr::from(([224,0,2,1],9090))).unwrap();
+        //sock.send_to(b"hello broadcast", SocketAddr::from(([255,255,255,255],9090))).unwrap();
         std::thread::sleep(std::time::Duration::from_millis(100));
         print!("send {} times\r",i+1);
         std::io::stdout().flush().unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::{Ipv4Addr, UdpSocket};
 fn main() {
     let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,9090)).unwrap();
-    println!("239,255,42,98 is mulitcast {}",Ipv4Addr::from([239,255,42,98]).is_multicast());
+    println!("224,0,2,1 is mulitcast {}",Ipv4Addr::from([224,0,2,1]).is_multicast());
     sock.set_broadcast(true).unwrap();
     sock.set_multicast_loop_v4(true).unwrap();
-    sock.join_multicast_v4(&Ipv4Addr::from([239,255,42,98]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    sock.join_multicast_v4(&Ipv4Addr::from([224,0,2,1]), &Ipv4Addr::UNSPECIFIED).unwrap();
     let mut buf = [0u8;u16::MAX as _];
     while let Ok((size,addr)) = sock.recv_from(& mut buf){
         println!("receive from {addr} content:{}",String::from_utf8_lossy(&buf[..size]));
