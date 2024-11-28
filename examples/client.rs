58ea24b103ce2627ed3f0413d4132836db7957bc
xmh0511 <970252187@qq.com>
2024-11-28 10:34:00 +0800

    u

--- examples/client.rs
+++ examples/client.rs
@@ -1,6 +1,6 @@
 use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
 fn main(){
-    let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,0)).unwrap();
+    let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,9090)).unwrap();
     let local_addr = sock.local_addr().unwrap();
     println!("local {local_addr}");
     sock.join_multicast_v4(&Ipv4Addr::from([234,2,2,2]), &Ipv4Addr::UNSPECIFIED).unwrap();
