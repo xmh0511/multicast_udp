57db40a9164f2606e750179ff7c84447b9de09f1
xmh0511 <970252187@qq.com>
2024-11-28 11:06:35 +0800

    u

--- examples/client.rs
+++ examples/client.rs
@@ -3,9 +3,9 @@ fn main(){
     let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,9090)).unwrap();
     let local_addr = sock.local_addr().unwrap();
     println!("local {local_addr}");
-    sock.join_multicast_v4(&Ipv4Addr::from([234,2,2,2]), &Ipv4Addr::UNSPECIFIED).unwrap();
-    for _ in 0..30{
-        sock.send_to(b"hello", SocketAddr::from(([234,2,2,2],9090))).unwrap();
+    sock.join_multicast_v4(&Ipv4Addr::from([239,255,42,98]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    for _ in 0..100{
+        sock.send_to(b"hello", SocketAddr::from(([239,255,42,98],9090))).unwrap();
     }
     println!("send over");
     sock.send_to(b"hello dedicated", SocketAddr::from(([192,168,1,158],9090))).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::{Ipv4Addr, UdpSocket};
 fn main() {
     let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED,9090)).unwrap();
-    sock.join_multicast_v4(&Ipv4Addr::from([234,2,2,2]), &Ipv4Addr::UNSPECIFIED).unwrap();
+    sock.join_multicast_v4(&Ipv4Addr::from([239,255,42,98]), &Ipv4Addr::UNSPECIFIED).unwrap();
     let mut buf = [0u8;u16::MAX as _];
     while let Ok((size,addr)) = sock.recv_from(& mut buf){
         println!("receive from {addr} content:{}",String::from_utf8_lossy(&buf[..size]));
